a6a4a71c4b3a2320899bbffb3c7b33175042bb7d
steveblackdoctor <steve@blackdoctor.com>
2026-04-06 11:12:36 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -224,7 +224,7 @@ const styles: Record<string, React.CSSProperties> = {
     flexDirection: 'column',
     position: 'relative' as const,
     borderRight: '1px solid #e0e0e0',
-    backgroundColor: WHITE,
+    backgroundColor: GOLD,
   },
   progressBar: {
     height: 4,
